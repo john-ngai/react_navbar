f65f58dd42e8c835e2454ba2a9753e78f71dcd8e
mrjohnming <john.ngai@hotmail.com>
2022-06-06 11:39:45 -0400

    Create navbar list & add hamburger icon

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 export default function Navbar() {
+  const hamburgerIcon = (
+    <FontAwesomeIcon className='faBars'
+      icon={faBars}
+    />
+  );
+
   return (
     <header>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+      <ul className="container--navbar">
+        <div className='navbar-links'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </div>
+        {hamburgerIcon}
       </ul>
     </header>
   );
